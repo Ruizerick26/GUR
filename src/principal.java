4222f0945daf07d599b379e258d5e2e5852f42ef
Ruizerick26 <ruizerick2525@gmail.com>
2023-07-07 11:57:35 -0500

    2.0

--- src/principal.java
+++ src/principal.java
@@ -1,6 +1,4 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class principal {
     public static void main(String[] args) {
@@ -8,10 +6,25 @@ public class principal {
         mclase miobjeto = new mclase("Erick",20);
         try(FileOutputStream fileout = new FileOutputStream(filepath);
             ObjectOutputStream abOut = new ObjectOutputStream(fileout);){
-            objOut.write(miobjeto);
+            abOut.writeObject(miobjeto);
             System.out.println("El archivo escrito correctamente");
         }catch (IOException e){
             throw new RuntimeException(e);
         }
+        try(
+                FileInputStream filen = new FileInputStream(filepath);
+                ObjectInputStream obin = new ObjectInputStream(filen);
+                )
+        {
+            mclase readobjeto=(mclase) obin.readObject();
+            System.out.println("El objeto en disco es: "+readobjeto);
+            System.out.println(readobjeto.getEdad());
+            System.out.println(readobjeto.getNombre());
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
