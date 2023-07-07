4eae7c379eeadd28ec0a732430cc50580b0ff3ee
Ruizerick26 <ruizerick2525@gmail.com>
2023-07-07 11:44:02 -0500

    datos

--- src/mclase.java
+++ src/mclase.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class mclase {
+import java.io.Serializable;
+
+public class mclase implements Serializable {
+    private String nombre;
+    private int edad;
+
+    public mclase(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString(){
+        return "Nombre:" +nombre+ ", edad"+edad;
+    }
+    private static final long serialVersionUID=1L;
 }
--- src/principal.java
+++ src/principal.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class principal {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class principal {
+    public static void main(String[] args) {
+        String filepath="datos.dat";
+        mclase miobjeto = new mclase("Erick",20);
+        try(FileOutputStream fileout = new FileOutputStream(filepath);
+            ObjectOutputStream abOut = new ObjectOutputStream(fileout);){
+            objOut.write(miobjeto);
+            System.out.println("El archivo escrito correctamente");
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
